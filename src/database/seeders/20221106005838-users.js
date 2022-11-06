639b89ecc80790f4116450e46488b6d0f8c0f232
Abraham Kamau <kirumba.kamau@gmail.com>
2022-11-06 03:20:32 +0200

    Creating seeders for users

--- src/database/seeders/20221106005838-users.js
+++ src/database/seeders/20221106005838-users.js
@@ -1,26 +1,14 @@
 'use strict';
 
+const data = require('../seeders-data/default-users');
+
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('users', data, {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('users', null, {});
   }
 };
